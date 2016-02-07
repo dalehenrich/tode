3747d8349f0a94391dfab6b439b5d8960dccf854
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-02-07 07:33:16 -0800

    project new command supprt

--- tests/testTode.sh
+++ tests/testTode.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# project new command tests need this
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
 $GS_HOME/bin/devKitCommandLine serverDoIt $1 << EOF
   | testReport |
   testReport := SmalltalkCI runCIFor: '$BASE/tests/smalltalkCI.ston'  produceXMLLog: false.
