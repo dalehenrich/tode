80c8206ce0a563dcab6b6f81f16103841a029330
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-02-07 06:31:04 -0800

    .travis.yml should fail if scripts fail ... fix the testTode.sh script

--- tests/testTode.sh
+++ tests/testTode.sh
@@ -4,10 +4,7 @@ set -ex
 
 $GS_HOME/bin/devKitCommandLine serverDoIt $1 << EOF
   | testReport |
-  (BinaryOrTextFile openReadOnServer: '${SMALLTALK_CI_HOME}/src/SmalltalkCI-Core.st')
-    fileIn;
-    close.
-  testReport := (Smalltalk at: #SmalltalkCI) runCIFor: '$BASE/tests/smalltalkCI.ston'  produceXMLLog: false.
+  testReport := SmalltalkCI runCIFor: '$BASE/tests/smalltalkCI.ston'  produceXMLLog: false.
   System commitTransaction.
   ((testReport suiteFailures > 0) or: [testReport suiteErrors > 0 ])
     ifTrue: [
