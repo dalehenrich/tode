03d59d8599509a9c3959d5415234489617a10ce5
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-02-08 13:04:53 -0800

    fix branch branching ... some more

--- tests/installGsDevKit_home.sh
+++ tests/installGsDevKit_home.sh
@@ -5,7 +5,7 @@ set -ex
 sudo hostname travis.dev
 $GS_HOME/bin/private/clone_sys_local -c https # pre-clone /sys/local, for travisCustomize.sh
 tests/travisCustomize.sh             # no backups on travis and use proposed tODE repo
-if [ "${GS_DEVKIT_HOME_BRANCH}" ="dev" ] ; then
+if [ "${GS_DEVKIT_HOME_BRANCH}" = "dev" ] ; then
   installServer
   createClient tode
   createStone travis $GS_VERSION
