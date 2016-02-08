6fa0059a205099be01450a720754c1f28439e070
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-02-08 12:54:49 -0800

    make changes so that travis tests run against master branch of GsDevKit_home

---
+++ tests/installGsDevKit_home.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+sudo hostname travis.dev
+$GS_HOME/bin/private/clone_sys_local -c https # pre-clone /sys/local, for travisCustomize.sh
+tests/travisCustomize.sh             # no backups on travis and use proposed tODE repo
+if [ "${GS_DEVKIT_HOME_BRANCH}" ="master" ] ; then
+  installServer
+  createClient tode
+  createStone travis $GS_VERSION
+else
+  $GS_HOME/utils/installOsPrereqs -X
+  installServerClient travis tode $GS_VERSION
+fi
+
+
+
+
