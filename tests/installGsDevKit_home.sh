ed5e6ce3d711e8bf151101157d49df4e3152b9b7
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-03-14 20:08:18 -0700

    dev branch hack for installing GsDevKit_home no longer needed

--- tests/installGsDevKit_home.sh
+++ tests/installGsDevKit_home.sh
@@ -5,14 +5,9 @@ set -ex
 sudo hostname travis.dev
 $GS_HOME/bin/private/clone_sys_local -c https # pre-clone /sys/local, for travisCustomize.sh
 tests/travisCustomize.sh             # no backups on travis and use proposed tODE repo
-if [ "${GS_DEVKIT_HOME_BRANCH}" = "dev" ] ; then
-  installServer
-  createClient tode
-  createStone travis $GS_VERSION
-else
-  $GS_HOME/bin/utils/installOsPrereqs -X
-  installServerClient travis tode $GS_VERSION
-fi
+installServer
+createClient tode
+createStone travis $GS_VERSION
 
 
 
