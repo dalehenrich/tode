26d0543dab4e987a7c819c1df0d801055b43a7cd
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2016-03-16 10:34:03 -0700

    fiddle installGsDevKit_home script

--- tests/installGsDevKit_home.sh
+++ tests/installGsDevKit_home.sh
@@ -5,7 +5,7 @@ set -ex
 sudo hostname travis.dev
 $GS_HOME/bin/private/clone_sys_local -c https # pre-clone /sys/local, for travisCustomize.sh
 tests/travisCustomize.sh             # no backups on travis and use proposed tODE repo
-installServer
+installServerClient
 createClient tode
 createStone travis $GS_VERSION
 
